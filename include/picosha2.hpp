8a7ebbc836d253e9c5cb7daff95d7d6a149782d8
porshdany <57595417+porshdany@users.noreply.github.com>
2020-05-22 21:28:52 +0300

    Update picosha2.hpp

--- include/picosha2.hpp
+++ include/picosha2.hpp
@@ -1,8 +1,7 @@
 /*
 The MIT License (MIT)
 
-Copyright (C) 2017 okdshin
-
+Copyright (C) 2020 Porshdany
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
